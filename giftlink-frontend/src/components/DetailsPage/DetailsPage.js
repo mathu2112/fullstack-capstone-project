9ed7da409e5b42973588cef002dc3eccbe1ddd23
mathu2112 <mathushawlika1221@gmail.com>
2025-09-21 11:55:13 +0400

    Update DetailsPage.js

--- giftlink-frontend/src/components/DetailsPage/DetailsPage.js
+++ giftlink-frontend/src/components/DetailsPage/DetailsPage.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
+import { urlConfig } from 'config';  // absolute import
 import './DetailsPage.css';
-import { urlConfig } from '../../config';
+
 
 function DetailsPage() {
     const navigate = useNavigate();
