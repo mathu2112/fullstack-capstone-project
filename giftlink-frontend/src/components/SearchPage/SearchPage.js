c7786210a3af13add7009819dcddfa5e14839efa
mathu2112 <mathushawlika1221@gmail.com>
2025-09-21 11:37:17 +0400

    Update SearchPage.js

--- giftlink-frontend/src/components/SearchPage/SearchPage.js
+++ giftlink-frontend/src/components/SearchPage/SearchPage.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {urlConfig} from '../../config';
+import { urlConfig } from '../config';
+
 
 function SearchPage() {
 
