153c74f66e64505c6fd6168b1c95c10a94e73efa
mathu2112 <mathushawlika1221@gmail.com>
2025-09-21 12:02:12 +0400

    Update MainPage.js

--- giftlink-frontend/src/components/MainPage/MainPage.js
+++ giftlink-frontend/src/components/MainPage/MainPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { urlConfig } from 'config';   // absolute import
+import { urlConfig } from '../../config';
 
 
 
